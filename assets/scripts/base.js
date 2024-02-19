e9674993d4daebd31d8449f7d137cf1ad17b6a21
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-02-19 19:06:48 +0600

    seperated date and time

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,8 +1,24 @@
 // silence is golden
 function updateDateTime() {
+    const dateDiv = document.querySelector('#date');
+    const clockDiv = document.querySelector('#clock');
+
+    if (!dateDiv || !clockDiv) {
+        console.error('Date or clock element not found');
+        return;
+    }
+
     const now = new Date();
-    const currentDateTime = now.toLocaleString();
-    document.querySelector('#clock').textContent = currentDateTime;
+
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+
+    const formattedDate = now.toLocaleDateString('en-US', dateOptions);
+    const formattedClock = now.toLocaleTimeString('en-US', timeOptions);
+
+    dateDiv.textContent = formattedDate;
+    clockDiv.textContent = formattedClock;
+
 }
 
 setInterval(updateDateTime, 1000);
