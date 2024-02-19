6d8cd191fdca42c35ae263f0b853684da976e6a2
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-02-19 18:21:08 +0600

    added clock

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,1 +1,8 @@
 // silence is golden
+function updateDateTime() {
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+    document.querySelector('#clock').textContent = currentDateTime;
+}
+
+setInterval(updateDateTime, 1000);
