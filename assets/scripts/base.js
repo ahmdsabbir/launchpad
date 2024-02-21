dc5c379a4438e0f60a3dd6a594aabfb98ebbe757
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-02-21 20:00:05 +0600

    wip: modified clock and date

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,53 +1,43 @@
-// silence is golden
+const dateDiv = document.querySelector('#date');
+const clockDiv = document.querySelector('#time');
+
 function updateDateTime() {
-    const dateDiv = document.querySelector('#date');
-    const clockDiv = document.querySelector('#clock');
+    const now = new Date();
 
-    if (!dateDiv || !clockDiv) {
-        console.error('Date or clock element not found');
-        return;
-    }
+    const weekday = now.toLocaleString('en-US', {
+        weekday: 'long'
+    });
+    const day = now.toLocaleString('en-US', {
+        day: 'numeric'
+    });
+    const month = now.toLocaleString('en-US', {
+        month: 'long'
+    });
+    const year = now.toLocaleString('en-US', {
+        year: 'numeric'
+    });
 
-    const weekDay = {weekday: 'long'}
-    const month = {month: 'long'}
-    const day = {year: 'numeric'}
-    const year = {year: 'numeric'}
+    const hour = now.toLocaleTimeString('en-US', { hour: '2-digit', hour12: true })
+    const minute = now.toLocaleString('en-US', {minute: 'numeric'})
+    const second = now.toLocaleString('en-US', {second: 'numeric'})
 
-    const hour = {hour: 'numeric'}
-    const minute = {minute: 'numeric'}
-    const second = {second: 'numeric'}
-
-    dateDiv.innerHTML = generateDateDiv(weekDay, month, day, year);
-    clockDiv.innerHTML = generateClockDiv(hour, minute, second);
-
+    dateDiv.innerHTML = genDateDiv(weekday, day, month, year);
+    clockDiv.innerHTML = genClockDiv(hour, minute, second);
 }
 
+function genDateDiv(weekday, day, month, year) {
+    return `
+    <span id="weekday">${weekday}</span>
+    <br>
+    <span id="month">${month}</span> <span id="day">${day}</span>, <span id="weekday">${weekday}</span>
+    `
+}
+
+function genClockDiv(hour, minute, second) {
+    return `
+    <span id="hour">${hour}</span> : <span id="minute">${minute}</span>:<span id="second">${second}</span>
+    `
+}
+
+updateDateTime();
 setInterval(updateDateTime, 1000);
-
-function generateDateDiv(weekDay, month, day, year) {
-    const now = new Date()
-    const formattedWeekDay = now.toLocaleDateString('en-US', weekDay);
-    const formattedMonth = now.toLocaleTimeString('en-US', month);
-    const formattedDay = now.toLocaleDateString('en-US', day);
-    const formattedYear = now.toLocaleDateString('en-US', year);
- 
-    return `
-    <div class="date weekDay">${formattedWeekDay}</div>
-    <div class="date month">${formattedMonth}</div>
-    <div class="date day">${formattedDay}</div>
-    <div class="date year">${formattedYear}</div>
-    `
-}
-
-function generateClockDiv(hour, minute, second) {
-    const now = new Date()
-    const formattedHour = now.toLocaleTimeString('en-US', hour);
-    const formattedMinute = now.toLocaleDateString('en-US', minute);
-    const formattedSecond = now.toLocaleTimeString('en-US', second);
-
-    return `
-    <div class="clock hour">${formattedHour}</div>
-    <div class="clock minute">${formattedMinute}</div>
-    <div class="clock second">${formattedSecond}</div>
-    `
-}
