11250b5ebfcf562657a98765294129fc6400b4c1
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-02-22 10:31:12 +0600

    wip: formatted base.js

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -4,20 +4,12 @@ const clockDiv = document.querySelector('#time');
 function updateDateTime() {
     const now = new Date();
 
-    const weekday = now.toLocaleString('en-US', {
-        weekday: 'long'
-    });
-    const day = now.toLocaleString('en-US', {
-        day: 'numeric'
-    });
-    const month = now.toLocaleString('en-US', {
-        month: 'long'
-    });
-    const year = now.toLocaleString('en-US', {
-        year: 'numeric'
-    });
+    const weekday = now.toLocaleString('en-US', {weekday: 'long'});
+    const day = now.toLocaleString('en-US', {day: 'numeric'});
+    const month = now.toLocaleString('en-US', {month: 'long'});
+    const year = now.toLocaleString('en-US', {year: 'numeric'});
 
-    const hour = now.toLocaleTimeString('en-US', { hour: '2-digit', hour12: true })
+    const hour = now.toLocaleTimeString('en-US', { hour: '2-digit', hour12: false })
     const minute = now.toLocaleString('en-US', {minute: 'numeric'})
     const second = now.toLocaleString('en-US', {second: 'numeric'})
 
@@ -29,14 +21,12 @@ function genDateDiv(weekday, day, month, year) {
     return `
     <span id="weekday">${weekday}</span>
     <br>
-    <span id="month">${month}</span> <span id="day">${day}</span>, <span id="weekday">${weekday}</span>
+    <span id="month">${month}</span> <span id="day">${day}</span>, <span id="year">${year}</span>
     `
 }
 
 function genClockDiv(hour, minute, second) {
-    return `
-    <span id="hour">${hour}</span> : <span id="minute">${minute}</span>:<span id="second">${second}</span>
-    `
+    return `<span id="hour">${hour}</span>:<span id="minute">${minute}</span>:<span id="second">${second}</span>`
 }
 
 updateDateTime();
