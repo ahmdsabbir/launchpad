c629d3c00846b7aae0869129ccff4f3b2f764136
ahmdsabbir <ahmd.sabbir@gmail.com>
2024-02-24 11:36:11 +0600

    wip: style changed

--- assets/scripts/bookmark.js
+++ assets/scripts/bookmark.js
@@ -1,6 +1,31 @@
-chrome.bookmarks.getTree((bookmarks) => {
-    console.log('#####', bookmarks)
-    bookmarks.forEach((bookmark) => {
-        console.log(bookmark.title, bookmark.url);
+function getBookmarks(callback) {
+    chrome.bookmarks.getTree((nodes) => {
+        const bookmarks = traverseBookmarks(nodes);
+        if (typeof callback === 'function') {
+            callback(bookmarks);
+        }
     });
+}
+
+function traverseBookmarks(nodes) {
+    let bookmarks = [];
+    for (let node of nodes) {
+        if (node.url) {
+            bookmarks.push({
+                title: node.title,
+                url: node.url
+            });
+        }
+        if (node.children) {
+            bookmarks = bookmarks.concat(traverseBookmarks(node.children));
+        }
+    }
+    return bookmarks;
+}
+
+
+
+// Example usage:
+getBookmarks((bookmarks) => {
+    console.log(bookmarks);
 });
